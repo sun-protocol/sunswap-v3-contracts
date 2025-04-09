82277ba136270a5c50e9eb5b94f77e4e4d92c0f6
Leon.Zhang <leon.zhang@tron.network>
2025-04-09 21:14:40 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# SunSwap V3 tron 部署：
+
+## usage
+
+### compile
+
+```sh
+$ truffle compile
+```
+
+### deploy
+
+```sh
+$ tronbox migrate --f 2 --to 2 --network nile
+```
+
---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mainnet: {
+      netId: 'mainnet',
+      rpcURL: 'https://api.trongrid.io',
+      WTRX: 'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR'
+      },
+    nile: {
+      netId: 'nile',
+      rpcURL: 'https://nile.trongrid.io',
+      //rpcURL: 'https://47.252.19.181:50051',
+      WTRX: 'TYsbWxNnyTgsZaTFaue9hqpxkU3Fkco94a'
+      },
+  };
