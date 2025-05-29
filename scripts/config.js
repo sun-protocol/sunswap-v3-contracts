f0d4188bd10e97c031984d44093b6a81a626a464
Leon.Zhang <leon.zhang@tron.network>
2025-05-29 18:55:59 +0800

    add deployed address

--- scripts/config.js
+++ scripts/config.js
@@ -2,12 +2,24 @@ module.exports = {
     mainnet: {
       netId: 'mainnet',
       rpcURL: 'https://api.trongrid.io',
-      WTRX: 'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR'
+      WTRX: 'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR',
+      UniswapV3Factory : 'TThJt8zaJzJMhCEScH7zWKnp5buVZqys9x',
+      Quoter: 'TLhZ48yfHygMLM2uZr87zJJusHjGen97gh',
+      SwapRouter: 'TQAvWQpT9H916GckwWDJNhYZvQMkuRL7PN',
+      NonfungiblePositionManager: 'TLSWrv7eC1AZCXkRjpqMZUmvgd99cj7pPF',
+      TickLens: 'TBBjWiPHouzEx2QRjBzTw9EA8YjG43XiAi',
+
       },
     nile: {
       netId: 'nile',
       rpcURL: 'https://nile.trongrid.io',
       //rpcURL: 'https://47.252.19.181:50051',
-      WTRX: 'TYsbWxNnyTgsZaTFaue9hqpxkU3Fkco94a'
+      WTRX: 'TYsbWxNnyTgsZaTFaue9hqpxkU3Fkco94a',
+      UniswapV3Factory : 'TUTGcsGDRScK1gsDPMELV2QZxeESWb1Gac',
+      Quoter: 'TUcM2gkpWEJxBpkweLdVoRp6DAUsw2vWR6',
+      SwapRouter: 'TFkswj6rUfK3cQtFGzungCkNXxD2UCpEVD',
+      NonfungiblePositionManager: 'TPQzqHbCzQfoVdAV6bLwGDos8Lk2UjXz2R',
+      TickLens: 'TXWuk11iagFK2qAWbBsP58A5ncU5L1LYq5',
+
       },
   };
